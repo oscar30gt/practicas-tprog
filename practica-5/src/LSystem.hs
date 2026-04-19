99e0e3138f093d88e03be1cda374eb9a1273d4e5
huugoogs <hugogs081006@gmail.com>
2026-04-19 15:11:44 +0200

    Practica 5(?

--- practica-5/src/LSystem.hs
+++ practica-5/src/LSystem.hs
@@ -1,4 +1,7 @@
-module LSystem (tplot, lsystem) where
+module LSystem (
+  tplot, lsystem, triangleCommands, squareCommands, circleCommands, ruleKoch, ruleMinkowski,
+  ruleSierpinskiTriangle, ruleKochSquare, ruleArrowhead, ruleHilbert, ruleGosper
+) where
 
 import Turtle
 
@@ -13,31 +16,69 @@ tplot tortuga ordenes = reverse (recorrer tortuga ordenes [obtenerPos tortuga])
     -- Función recursiva para recorrer las órdenes de la tortuga y acumular las posiciones
     recorrer :: Turtle -> String -> [Position] -> [Position]
     recorrer tortugaActual [] puntos = puntos -- Si no hay más órdenes, devuelve las posiciones acumuladas
-
-    recorrer tortugaActual (c:cs) puntos =
-      case c of
-        '>' ->    -- Si la orden es '>'
-          let nuevaTortuga = moveTurtle tortugaActual Forward -- Mover hacia adelante
-          in recorrer nuevaTortuga cs (obtenerPos nuevaTortuga : puntos)  -- Agregar la nueva posición a la lista de puntos
-        '+' ->    -- Si la orden es '+'
-          recorrer (moveTurtle tortugaActual TurnRight) cs puntos -- Girar a la derecha y continuar con las órdenes restantes
-        '-' ->    -- Si la orden es '-'
-          recorrer (moveTurtle tortugaActual TurnLeft) cs puntos -- Girar a la izquierda y continuar con las órdenes restantes
-        letra ->  -- En cualquier otro caso
-          if letra >= 'A' && letra <= 'Z' -- Si la letra es una letra mayúscula
-          then
-            let nuevaTortuga = moveTurtle tortugaActual Forward -- Mover hacia adelante
-            in recorrer nuevaTortuga cs (obtenerPos nuevaTortuga : puntos)  -- Agregar la nueva posición a la lista de puntos
-          else  -- Si la letra no es una letra mayúscula
-            recorrer tortugaActual cs puntos  -- Continuar con las órdenes restantes sin mover la tortuga
+    recorrer tortugaActual (c:cs) puntos
+      | c == '>'  =  -- Si la orden es '>'
+          let nuevaTortuga = moveTurtle tortugaActual Forward -- Mueve la tortuga hacia adelante
+          in recorrer nuevaTortuga cs (obtenerPos nuevaTortuga : puntos)  -- Agrega la nueva posición a la lista de puntos y continúa recorriendo
+      | c == '+'  =  -- Si la orden es '+'
+          recorrer (moveTurtle tortugaActual TurnRight) cs puntos -- Gira la tortuga a la derecha y continúa recorriendo
+      | c == '-'  =  -- Si la orden es '-'
+          recorrer (moveTurtle tortugaActual TurnLeft) cs puntos  -- Gira la tortuga a la izquierda y continúa recorriendo
+      | c >= 'A' && c <= 'Z' =  -- Si la orden es una letra mayúscula (representa un comando de movimiento)
+          let nuevaTortuga = moveTurtle tortugaActual Forward -- Mueve la tortuga hacia adelante
+          in recorrer nuevaTortuga cs (obtenerPos nuevaTortuga : puntos)  -- Agrega la nueva posición a la lista de puntos y continúa recorriendo
+      | otherwise = -- Si la orden no es reconocida
+          recorrer tortugaActual cs puntos  -- Continúa recorriendo sin cambiar la tortuga ni agregar puntos
 
 -- Función para generar la cadena resultante del sistema L a partir de las reglas, el axioma y el número de iteraciones
 lsystem :: (Char -> String) -> String -> Int -> String
-lsystem reglas axioma veces
-  | veces <= 0 = axioma -- Si el número de iteraciones es 0 o negativo, devuelve el axioma sin cambios
-  | otherwise = lsystem reglas (reescribir reglas axioma) (veces - 1) -- Si no, reescribe el axioma y llama recursivamente
+lsystem reglas axioma 0 = axioma -- Si el número de iteraciones es 0, devuelve el axioma sin cambios
+lsystem reglas axioma n = lsystem reglas (reescribir reglas axioma) (n - 1) -- Si no, reescribe el axioma y llama recursivamente
 
 -- Función auxiliar para reescribir la cadena según las reglas dadas
 reescribir :: (Char -> String) -> String -> String
 reescribir reglas [] = [] -- Si la cadena está vacía, devuelve una cadena vacía
 reescribir reglas (x:xs) = reglas x ++ reescribir reglas xs -- Reescribe el primer carácter según las reglas y luego reescribe el resto de la cadena
+
+-- Funciones para generar comandos de figuras básicas
+triangleCommands :: String
+triangleCommands = ">+>+>+"
+
+squareCommands :: String
+squareCommands = ">+>+>+>+"
+
+circleCommands :: Int -> String
+circleCommands sides = concat (replicate sides ">+")
+
+-- Reglas para diferentes sistemas L
+ruleKoch :: Char -> String
+ruleKoch 'F' = "F-F++F-F"
+ruleKoch c = [c]
+
+ruleMinkowski :: Char -> String
+ruleMinkowski 'F' = "F-F+F+FF-F-F+F"
+ruleMinkowski c = [c]
+
+ruleSierpinskiTriangle :: Char -> String
+ruleSierpinskiTriangle 'F' = "F-G+F+G-F"
+ruleSierpinskiTriangle 'G' = "GG"
+ruleSierpinskiTriangle c = [c]
+
+ruleKochSquare :: Char -> String
+ruleKochSquare 'F' = "F-F+F+F-F"
+ruleKochSquare c = [c]
+
+ruleArrowhead :: Char -> String
+ruleArrowhead 'F' = "G+F+G"
+ruleArrowhead 'G' = "F-G-F"
+ruleArrowhead c = [c]
+
+ruleHilbert :: Char -> String
+ruleHilbert 'f' = "-g>+f>f+>g-"
+ruleHilbert 'g' = "+f>-g>g->f+"
+ruleHilbert c = [c]
+
+ruleGosper :: Char -> String
+ruleGosper 'F' = "F-G--G+F++FF+G-"
+ruleGosper 'G' = "+F-GG--G-F++F+G"
+ruleGosper c = [c]
--- practica-5/src/main.hs
+++ practica-5/src/main.hs
@@ -1,82 +1,18 @@
 import LSystem
 import SVG
-import Turtle
-
--- Funciones para generar comandos de figuras básicas
-triangleCommands :: String
-triangleCommands = ">+>+>+"
-
-squareCommands :: String
-squareCommands = ">+>+>+>+"
-
-circleCommands :: Int -> String
-circleCommands sides = concat (replicate sides ">+")
-
--- Reglas para diferentes sistemas L
-ruleKoch :: Char -> String
-ruleKoch 'F' = "F-F++F-F"
-ruleKoch c = [c]
-
-ruleMinkowski :: Char -> String
-ruleMinkowski 'F' = "F-F+F+FF-F-F+F"
-ruleMinkowski c = [c]
-
-ruleSierpinskiTriangle :: Char -> String
-ruleSierpinskiTriangle 'F' = "F-G+F+G-F"
-ruleSierpinskiTriangle 'G' = "GG"
-ruleSierpinskiTriangle c = [c]
-
-ruleKochSquare :: Char -> String
-ruleKochSquare 'F' = "F-F+F+F-F"
-ruleKochSquare c = [c]
-
-ruleArrowhead :: Char -> String
-ruleArrowhead 'F' = "G+F+G"
-ruleArrowhead 'G' = "F-G-F"
-ruleArrowhead c = [c]
-
-ruleHilbert :: Char -> String
-ruleHilbert 'f' = "-g>+f>f+>g-"
-ruleHilbert 'g' = "+f>-g>g->f+"
-ruleHilbert c = [c]
-
-ruleGosper :: Char -> String
-ruleGosper 'F' = "F-G--G+F++FF+G-"
-ruleGosper 'G' = "+F-GG--G-F++F+G"
-ruleGosper c = [c]
-
--- Funciones para guardar figuras y sistemas L en archivos SVG
-saveFigureCommands :: String -> Turtle -> String -> IO ()
-saveFigureCommands = saveFigure
-
-saveGenerated :: String -> Turtle -> (Char -> String) -> String -> Int -> IO ()
-saveGenerated = saveLSystemFigure
-
-saveFigure :: String -> Turtle -> String -> IO ()
-saveFigure name turtle commands = savesvg name (tplot turtle commands)
-
-saveLSystemFigure :: String -> Turtle -> (Char -> String) -> String -> Int -> IO ()
-saveLSystemFigure name turtle rules axiom iterations =
-  savesvg name (tplot turtle (lsystem rules axiom iterations))
 
 main :: IO ()
 main = do
-  -- Guardar figuras básicas
-  saveFigure "triangulo" (1, 120, (0, 0), 0) triangleCommands
-  saveFigure "cuadrado" (1, 90, (0, 0), 0) squareCommands
-  saveFigure "circulo" (1, 10, (0, 0), 0) (circleCommands 36)
-
--- Guardar figuras generadas por sistemas L
-  saveLSystemFigure "koch" (1, 60, (0, 0), 0) ruleKoch "F" 3
-  saveLSystemFigure "koch_snowflake" (1, 60, (0, 0), 0) ruleKoch "F++F++F" 3
-  saveLSystemFigure "koch_cuadrada" (1, 90, (0, 0), 0) ruleKochSquare "F" 3
-  saveLSystemFigure "minkowski" (1, 90, (0, 0), 0) ruleMinkowski "F+F+F+F" 2
-  saveLSystemFigure "sierpinski_triangle" (1, 120, (0, 0), 0) ruleSierpinskiTriangle "F-G-G" 3
-  saveLSystemFigure "arrowhead" (1, 60, (0, 0), 0) ruleArrowhead "F" 4
-  saveLSystemFigure "hilbert" (1, 90, (0, 0), 0) ruleHilbert "f" 4
-  saveLSystemFigure "gosper" (1, 60, (0, 0), 0) ruleGosper "F" 3
-
--- Imprimir mensaje de confirmación
-  putStrLn "SVG files generated."
-
-
+  -- Figuras básicas
+  savesvg "triangulo" (tplot (1,120,(0,0),0) triangleCommands)
+  savesvg "cuadrado" (tplot (1,90,(0,0),0) squareCommands)
+  savesvg "circulo" (tplot (1,10,(0,0),0) (circleCommands 36))
+  -- L-Systems
+  savesvg "koch" (tplot (1,60,(0,0),0) (lsystem ruleKoch "F" 3))
+  savesvg "koch_snowflake" (tplot (1,60,(0,0),0) (lsystem ruleKoch "F++F++F" 3))
+  savesvg "koch_cuadrada" (tplot (1,90,(0,0),0) (lsystem ruleKochSquare "F" 3))
+  savesvg "minkowski" (tplot (1,90,(0,0),0) (lsystem ruleMinkowski "F+F+F+F" 2))
+  savesvg "sierpinski_triangle" (tplot (1,120,(0,0),0) (lsystem ruleSierpinskiTriangle "F-G-G" 3))
+  savesvg "arrowhead" (tplot (1,60,(0,0),0) (lsystem ruleArrowhead "F" 4))
+  savesvg "hilbert" (tplot (1,90,(0,0),0) (lsystem ruleHilbert "f" 4))
+  savesvg "gosper" (tplot (1,60,(0,0),0) (lsystem ruleGosper "F" 3))
